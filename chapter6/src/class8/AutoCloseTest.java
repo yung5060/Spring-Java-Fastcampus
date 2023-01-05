954f88c8f02e79463c0b090a988494c56bb6b69a
Yung Cho <yung5060@kakao.com>
2023-01-05 22:35:31 +0900

    try/catch2

--- chapter6/src/class8/AutoCloseTest.java
+++ chapter6/src/class8/AutoCloseTest.java
@@ -2,16 +2,16 @@ package class8;
 
 public class AutoCloseTest {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		AutoCloseable obj = new AutoClosableObj();
 
-        AutoCloseable obj = new AutoClosableObj();
+		try (obj) {
+			throw new Exception();
+		} catch (Exception e) {
+			System.out.println("exception");
+		}
 
-        try (obj) {
-        	throw new Exception();
-        } catch (Exception e) {
-            System.out.println("exception");
-        }
+		System.out.println("end");
 
-        System.out.println("end");
-    }
+	}
 }
